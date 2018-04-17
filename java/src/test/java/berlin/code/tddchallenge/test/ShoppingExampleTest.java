ca0a79b52a058001c54b252086e9f763b36570b9
Martin Dreher <luma.inc@gmail.com>
2018-04-17 14:56:18 +0200

    [java] improve test structure

--- java/src/test/java/berlin/code/tddchallenge/test/ShoppingExampleTest.java
+++ java/src/test/java/berlin/code/tddchallenge/test/ShoppingExampleTest.java
@@ -11,23 +11,34 @@ import java.io.IOException;
 import static org.junit.jupiter.api.Assertions.*;
 
 public class ShoppingExampleTest {
+    private ShoppingCart cart;
+    private Database db;
 
-    @Test
-    public void shoppingTest() throws IOException {
-        Database db = new Database();
-        ShoppingCart cart = new ShoppingCart(
+    @BeforeEach
+    public void setup() throws IOException {
+        db = new Database();
+        cart = new ShoppingCart(
                 new ShoppingCartItem("apple", 5),
                 new ShoppingCartItem("orange", 4),
                 new ShoppingCartItem("milk", 2),
                 new ShoppingCartItem("red wine", 3),
                 new ShoppingCartItem("stamps", 4)
         );
+    }
 
+    @Test
+    public void productsAvailableTest() {
         for (ShoppingCartItem item : cart.getContents()) {
             assertNotNull(db.getProduct(item.getProductId()), "product lookup failed for " + item);
         }
+    }
 
+    @Test
+    public void taxCalculatorTest() {
         TaxCalculator calc = new TaxCalculator(db, cart);
+
+        // TODO write integration test
+
         assertEquals(2.89, calc.getSubtotalTax(.07), 0.005, "unexpected subtotal for 7%");
     }
 }
