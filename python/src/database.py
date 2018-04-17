34b9bde2fa7b21707e7a2ce0cd993d7b0403bcd7
Martin Dreher <luma.inc@gmail.com>
2018-04-17 23:17:40 +0200

    [python] find json file even if script is being called from another directory

--- python/src/database.py
+++ python/src/database.py
@@ -1,10 +1,14 @@
 import json
+import os
+
 
 class Database:
-    def __init__(self, fileName="../products.json"):
-        with open(fileName) as f:
+    def __init__(self, file_name=None):
+        if file_name is None:
+            file_name = os.path.join(os.path.dirname(__file__), "../../products.json")
+        with open(file_name) as f:
             products = json.load(f)
-        self.products_by_id = {p['productId'] : p for p in products}
+        self.products_by_id = {p['productId']: p for p in products}
 
     def get_product(self, product_id):
         return self.products_by_id[product_id]
