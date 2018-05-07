41bbed0d45abf96ebd1c63b1bdea4b6a21b35090
Piero Hernandez <jeanpierogm@gmail.com>
2018-05-07 14:46:02 +0200

    add test to check all records are valid

--- python/src/database_test.py
+++ python/src/database_test.py
@@ -8,3 +8,10 @@ def test_access_database():
     assert sugar['price'] == 0.65
     assert sugar['taxRate'] == 0.07
     assert db.get_product("red wine") is not None
+
+
+def test_all_valid_records():
+    db = Database()
+    for k, v in db.products_by_id.items():
+        assert type(v.get('price')) == float
+        assert type(v.get('price')) == float
